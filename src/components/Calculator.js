620fd07dc9287747afad489ef8e2e5e272b61cfd
LOctavio <luis_ram1997@hotmail.com>
2021-08-17 14:31:29 -0500

    Add className to each row

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,32 +11,32 @@ class Calculator extends React.Component {
     const { total } = this.props;
     return (
       <div className="calculator">
-        <input type="text" id="calculator-input" value={total} />
-        <div>
+        <input type="text" className="calculator-input" value={total} />
+        <div className="first-row">
           <button type="button">AC</button>
           <button type="button">+/-</button>
           <button type="button">%</button>
           <button type="button">÷</button>
         </div>
-        <div>
+        <div className="second-row">
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
           <button type="button">x</button>
         </div>
-        <div>
+        <div className="third-row">
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
           <button type="button">-</button>
         </div>
-        <div>
+        <div className="fourth-row">
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
           <button type="button">+</button>
         </div>
-        <div>
+        <div className="fifth-row">
           <button type="button">0</button>
           <button type="button">.</button>
           <button type="button">=</button>
