2d163fbc83e9169369938124c29117d0274c1c6c
LOctavio <luis_ram1997@hotmail.com>
2021-08-19 13:52:02 -0500

    Refactor to a functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,78 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      operationOutput: null,
-    };
-    this.addNumber = this.addNumber.bind(this);
-    this.addOperation = this.addOperation.bind(this);
-  }
-
-  addNumber(event) {
-    const { next } = calculate(this.state, event.target.textContent);
-    this.setState({ next });
-    const { total, operation } = this.state;
-    this.setState({
-      operationOutput: (total && operation && next) ? (total + operation + next) : null,
-    });
-  }
-
-  addOperation(event) {
-    const { total, next, operation } = calculate(this.state, event.target.textContent);
-    this.setState({ total, next, operation });
-    this.setState({ operationOutput: (total && operation) ? (total + operation) : null });
-  }
-
-  render() {
-    const { next, total, operationOutput } = this.state;
-    const { defaultValue } = this.props;
-    return (
-      <div className="calculator">
-        <input type="text" className="calculator-input" value={operationOutput || next || total || defaultValue} onChange={this.addNumber || this.addOperation} />
-        <div className="first-row">
-          <button type="button" onClick={this.addOperation}>AC</button>
-          <button type="button" onClick={this.addNumber}>+/-</button>
-          <button type="button" onClick={this.addOperation}>%</button>
-          <button type="button" onClick={this.addOperation}>÷</button>
-        </div>
-        <div className="second-row">
-          <button type="button" onClick={this.addNumber}>7</button>
-          <button type="button" onClick={this.addNumber}>8</button>
-          <button type="button" onClick={this.addNumber}>9</button>
-          <button type="button" onClick={this.addOperation}>x</button>
-        </div>
-        <div className="third-row">
-          <button type="button" onClick={this.addNumber}>4</button>
-          <button type="button" onClick={this.addNumber}>5</button>
-          <button type="button" onClick={this.addNumber}>6</button>
-          <button type="button" onClick={this.addOperation}>-</button>
-        </div>
-        <div className="fourth-row">
-          <button type="button" onClick={this.addNumber}>1</button>
-          <button type="button" onClick={this.addNumber}>2</button>
-          <button type="button" onClick={this.addNumber}>3</button>
-          <button type="button" onClick={this.addOperation}>+</button>
-        </div>
-        <div className="fifth-row">
-          <button type="button" onClick={this.addNumber}>0</button>
-          <button type="button" onClick={this.addNumber}>.</button>
-          <button type="button" onClick={this.addOperation}>=</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-Calculator.propTypes = {
-  defaultValue: PropTypes.string.isRequired,
+const addNumber = (event, setNext, setOperationOutput, args) => {
+  const { next } = calculate(args, event.target.textContent);
+  setNext(next);
+  const { total, operation } = args;
+  setOperationOutput((total && operation && next) ? (total + operation + next) : null);
 };
 
+const addOperation = (event, setTotal, setNext, setOperation, setOperationOutput, args) => {
+  const { total, next, operation } = calculate((args), event.target.textContent);
+  setTotal(total);
+  setNext(next);
+  setOperation(operation);
+  setOperationOutput((total && operation) ? (total + operation) : null);
+};
+
+const Calculator = (props) => {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [operationOutput, setOperationOutput] = useState(null);
+  const initValue = props;
+  const args = {
+    total, next, operation, operationOutput,
+  };
+  return (
+    <div className="calculator">
+      <input type="text" className="calculator-input" value={operationOutput || next || total || initValue.defaultValue} onChange={addNumber || addOperation} />
+      <div className="first-row">
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>AC</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>+/-</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>%</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>÷</button>
+      </div>
+      <div className="second-row">
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>7</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>8</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>9</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>x</button>
+      </div>
+      <div className="third-row">
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>4</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>5</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>6</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>-</button>
+      </div>
+      <div className="fourth-row">
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>1</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>2</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>3</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>+</button>
+      </div>
+      <div className="fifth-row">
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>0</button>
+        <button type="button" onClick={(event) => addNumber(event, setNext, setOperationOutput, args)}>.</button>
+        <button type="button" onClick={(event) => addOperation(event, setTotal, setNext, setOperation, setOperationOutput, args)}>=</button>
+      </div>
+    </div>
+  );
+};
 export default Calculator;
