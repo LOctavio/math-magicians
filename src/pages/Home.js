1b173a118a7df99304519adee738e190f2d1fda9
LOctavio <luis_ram1997@hotmail.com>
2021-08-26 09:43:43 -0500

    Add new Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to our page</h2>
+    <p>
+      <span>Reprehenderit sit nisi officia mollit magna.</span>
+      <span>Laboris eiusmod laborum ullamco sint laborum culpa aliqua in et.</span>
+      <span>Tempor ut veniam anim voluptate id nulla.</span>
+      <span>Cupidatat ad aliquip voluptate amet consectetur ad.</span>
+      <span>Exercitation cupidatat incididunt adipisicing occaecat enim sint.</span>
+    </p>
+    <p>
+      <span>Fugiat quis ex mollit excepteur.</span>
+      <span>Proident amet Lorem amet commodo do ex Lorem enim enim excepteur culpa.</span>
+      <span>Labore fugiat magna ea ullamco commodo minim id.</span>
+      <span>Cillum et nisi eiusmod ea nisi dolor culpa sunt elit elit.</span>
+      <span>Non mollit adipisicing mollit ipsum fugiat laboris ipsum incididunt fugiat.</span>
+    </p>
+  </div>
+);
+
+export default Home;
